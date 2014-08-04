bc06e3981a59eb41d0b17d2bd89709469c389b2f
George Jiglau <george@mux.ro>
2014-08-04 12:32:02 +0300

    Add more aliases

--- zsh/zsh/conf/aliases.zsh
+++ zsh/zsh/conf/aliases.zsh
@@ -1,9 +1,17 @@
+# vim
+alias v=vim
+alias svi='sudo vim'
+
+# python
 alias py=python
 alias py3=python3
 alias ipy=ipython
 alias ipy3=ipython3
 
-alias svi='sudo vi'
-
+# apt-get
+alias i='sudo apt-get install'
+alias purge='sudo apt-get purge'
+alias asearch='apt-cache search'
+alias ashow='apt-cache show'
 alias sag='sudo apt-get'
 alias upgrade="sudo sh -c 'apt-get update; apt-get dist-upgrade'"
