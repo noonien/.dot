c35ab8e625b6b1e59d38db5e727df2a840017782
George Jiglau <george@mux.ro>
2018-02-06 13:45:52 +0000

    [i3] Changed DE to xfce

--- i3/i3/utils/term.sh
+++ i3/i3/utils/term.sh
@@ -1,33 +1,23 @@
 #!/bin/bash
 # Start a terminal from the current focused terminal's CWD
 
-# Get ID of current focused window
-ID=$(xdpyinfo | grep focus | cut -f4 -d " ")
+# default CWD
+CWD=$HOME
 
-# Check if it's an urxvt window, if not, don't use it's CWD
-CLASS=$(xprop -id $ID | grep -m 1 CLASS | cut -d \" -f 2)
-if [ "$CLASS" != "urxvt" ]; then
-    exec urxvt
-fi
+# Get ID of current focused window
+ID=$(xdpyinfo | grep focus | cut -f4 -d " " | tr -d '[:space:],')
 
 # Get the process' PID
 PID=$(xprop -id $ID | grep -m 1 PID | cut -d " " -f 3)
 if [ -z "$PID" ]; then
-    exec urxvt
+    exec xfce4-terminal --disable-server
 fi
 
-TREE=$(pstree -lpA $PID | tail -n 1)
-PID=$(echo $TREE | awk -F'---' '{print $NF}' | sed -re 's/[^0-9]//g')
-
-if [[ "$1" == "--connect-ssh" && $(cat /proc/$PID/comm) == "ssh" ]]; then
-    SSH_CMD=$(cat /proc/$PID/cmdline | tr '\000' ' ')
-    SSH_CWD=$(xprop -id $ID | grep "WM_NAME(STRING)" | cut -d \  -f 4 | tr -d \")
-    exec urxvt -e $SSH_CMD -t  "cd $SSH_CWD; exec $SHELL"
+# Check if it's an xfce4-terminal window, if not, don't use it's CWD
+CLASS=$(xprop -id $ID | grep -m 1 CLASS | cut -d \" -f 2)
+if [ "$CLASS" == "xfce4-terminal" ]; then
+    SHELL_PID=$(ps -o pid --no-headers --ppid $PID | tr -d '[:space:]')
+    CWD=$(readlink /proc/$SHELL_PID/cwd)
 fi
 
-# If we find the working directory, run the command in that directory
-if ! [ -e "/proc/$PID/cwd" ]; then
-    exec urxvt
-fi
-
-exec urxvt -cd $(readlink /proc/$PID/cwd)
+exec xfce4-terminal --disable-server --working-directory="$CWD"
