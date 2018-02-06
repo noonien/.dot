14cfbd09387caabcba1c5ec699698a3dcdf5dd89
George Jiglau <george@mux.ro>
2018-02-06 13:47:23 +0000

    [zgen] Add helm OMZ module

--- zsh/zsh/conf/zgen.zsh
+++ zsh/zsh/conf/zgen.zsh
@@ -27,8 +27,10 @@ if ! zgen saved; then
 
     zgen oh-my-zsh plugins/git
     zgen oh-my-zsh plugins/git-extras
+
     zgen oh-my-zsh plugins/docker
     zgen oh-my-zsh plugins/kubectl
+    zgen oh-my-zsh plugins/helm
 
     zgen oh-my-zsh plugins/golang
     zgen oh-my-zsh plugins/python
