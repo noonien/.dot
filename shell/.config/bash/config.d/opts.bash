4c94554ea4de740dc2dc89bac8eeae11d69cb08f
George-Cristian Jiglau <george@mux.ro>
2020-01-14 14:33:31 +0000

    [bash] enable better tab completion

--- shell/.config/bash/config.d/opts.bash
+++ shell/.config/bash/config.d/opts.bash
@@ -20,3 +20,13 @@ shopt -s globstar
 # filename completion.  This changes the contents of the readline  editing  buffer.   If  not
 # set, bash attempts to preserve what the user typed.
 shopt -s direxpand
+
+# If there are multiple matches for completion, Tab should cycle through them
+bind 'TAB':menu-complete
+
+# Display a list of the matching files
+bind "set show-all-if-ambiguous on"
+
+# Perform partial completion on the first Tab press,
+# only start cycling full results on the second Tab press
+bind "set menu-complete-display-prefix on"
