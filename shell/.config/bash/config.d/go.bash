42878e0098a296a4e874d7697e9ff1df61c3cca3
George-Cristian Jiglau <george@mux.ro>
2020-02-27 11:22:37 +0000

    [go] more privacy please

--- shell/.config/bash/config.d/go.bash
+++ shell/.config/bash/config.d/go.bash
@@ -1,6 +1,7 @@
 # no tracking please
-export GONOSUMDB=*
-export GONOPROXY=*
+export GONOSUMDB='*'
+export GONOPROXY='*'
+export GOPRIVATE='*'
 
 if [[ -d "$HOME/.go" ]]; then
     export GOPATH=$HOME/.go
